76424a7e7eee5b9c597d98db39d2c03ab9d40e1e
Vijayendra K <43803349+VijayK1990@users.noreply.github.com>
2018-10-04 13:58:53 +0530

    Conversion Snippet for Minutes to Hours format

---
+++ vkj.minutes2hours.js
@@ -0,0 +1,11 @@
+/*
+	* FUNCTION DATE: 04-10-2018
+	* WRITTEN BY: VIJAYENDRA K
+	* PURPOSE: TO CONVERT MINUTES TO HOURS FORMAT
+*/
+function GetDuration(t) {            
+	var hours = Math.floor(parseInt(t) / 60);
+	var minutes = (parseInt(t) % 60);
+	var conv2 = ((('' + hours).length < 2 ? '0' : '') + hours) + ':' + ((('' + minutes).length < 2 ? '0' : '') + minutes);
+	return conv2;
+}
